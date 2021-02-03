e14bea9ddf195032bf5f6b06739b1dada158b13b
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro-2.local>
2021-02-03 14:07:28 -0500

    heroku

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const auth = require('json-server-auth')
 const app = jsonServer.create();
 const router = jsonServer.router('db.json')
 
+app.db = router.db
 
 app.use(auth)
 app.use(router)
 app.listen(3000)
 
-app.db = router.db
+
 //binding app to db
 
 
+app.use(router);
 
 
 
