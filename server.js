0109c1a4531f9234c5233f99554bfd93af453379
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro-2.local>
2021-02-03 14:35:20 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 
-const app = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-app.db = router.db
-
-app.use(auth)
-app.use(router)
-app.listen(3000)
+server.db = router.db
+server.use(auth)
+server.use('/api', router)
+server.listen(3000)
+server.use(middlewares)
 
 
 //binding app to db
 
 
-app.use(router);
+// server.use(router);
+// const app = jsonServer.create();
+// const router = jsonServer.router('db.json')
+
+// app.db = router.db
+// app.use(auth)
+// app.use(router)
+// app.listen(3000)
+
+
+//binding app to db
 
 
 
 
 
-app.listen(port, () => console.log(`\n** Running on port ${port} **\t http://localhost:${port}/\n`));
+
+server.listen(port, () => console.log(`\n** Running on port ${port} **\t http://localhost:${port}/\n`));
